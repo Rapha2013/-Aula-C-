88efeaf4d367edbf88be7ed7a339664499e8f427
Raphael <rapha_2gtba@hotmail.com>
2021-08-08 21:54:59 -0300

    Commit Inicial

---
+++ PrimeiroApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeiroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int i = 5;
+            int j = 2;
+            Console.WriteLine("i = " + i);
+            Console.WriteLine("j = " + j);
+
+
+            if (i > j){
+                Console.WriteLine("i" + " É maior que " + "j");
+            }else {
+                Console.WriteLine("j" + " É maior que " + "i");
+
+            }
+
+        
+        }
+
+    }
+}
